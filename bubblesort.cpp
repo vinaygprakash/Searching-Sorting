5e25005c4470a043cf376c9d3e0f2c7451bf99ed
vinaygprakash <116455167+vinaygprakash@users.noreply.github.com>
2023-01-17 17:18:28 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,32 @@
+/*
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+   for(int i=0;i<n-1;i++)
+   {
+       //no of loop it is n-1 times we have to do
+       for (int j=0;j<n-1-i;j++)
+           //this is for element
+       if(arr[j]>arr[j+1])
+       {
+           swap(arr[j],arr[j+1]);
+       }
+   }
+}
+
+
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+   for(int i=1;i<n;i++)
+   {
+       //no of loop it is n-1 times we have to do
+       for (int j=0;j<n-i;j++)
+           //this is for element
+       if(arr[j]>arr[j+1])
+       {
+           swap(arr[j],arr[j+1]);
+       }
+   }
+}
+*/
